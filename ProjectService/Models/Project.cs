f16adfac0d895d2d8284635fcc0b8681bbfa54ee
gayantharaka <a.k.g.t.damayantha@gmail.com>
2024-11-19 16:18:11 +0530

    Project model file added.

---
+++ ProjectService/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectService.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
